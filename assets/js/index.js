a8a4278eb7f44db6a02b006a3615feb83171959f
Bikal Rai <bikalrai2785@gmailcom>
2025-07-17 10:23:59 +0545

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+const hamburgerEl = document.querySelector('nav .hamburger');
+const navEl = document.querySelector('nav .nav');
+
+window.addEventListener('resize', () => {
+  let viewWidth = window.innerWidth;
+
+  if (viewWidth < 1130) {
+    hamburgerEl.style.display = 'block';
+    navEl.style.display = 'none';
+    navEl.style.transform = 'scale(0)';
+  } else {
+    hamburgerEl.style.display = 'none';
+    navEl.style.display = 'flex';
+    navEl.style.transform = 'scale(1)';
+  }
+});
+
+document.querySelector('header').addEventListener('click', function (e) {
+  if (e.target.classList.contains('hamburger__bar')) {
+    hamburgerEl.style.display = 'none';
+    navEl.style.display = 'flex';
+    navEl.style.transform = 'scale(1)';
+  }
+
+  if (e.target.classList.contains('close__btn')) {
+    hamburgerEl.style.display = 'block';
+    navEl.style.display = 'none';
+    navEl.style.transform = 'scale(0)';
+  }
+});
